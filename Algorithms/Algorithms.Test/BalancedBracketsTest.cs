de8e49ef6eaeeae8da077148daf6a235d8feb326
Ahmad Saeed <ahmadsaeed90@gmail.com>
2019-04-20 15:44:48 +0700

    add balanced brackets solver

---
+++ Algorithms/Algorithms.Test/BalancedBracketsTest.cs
@@ -0,0 +1,43 @@
+﻿using Algorithms.stack;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Test
+{
+    public class BalancedBracketsTest
+    {
+        private IBalancedBracketsSolver _balancedBracketsSolver;
+
+        [SetUp]
+        public void Setup()
+        {
+            _balancedBracketsSolver = new BalancedBracketsSolver();
+        }
+
+        [Test]
+        [TestCase("{[()]}")]
+        [TestCase("{{[[(())]]}}")]
+        [TestCase("[](){()}")]
+        [TestCase("()")]
+        [TestCase("({}([][]))[]()")]
+        public void When_BalancedBrackets_Should_ReturnYes(string input)
+        {
+            Assert.AreEqual("YES", _balancedBracketsSolver.IsBalanced(input));
+        }
+
+        [Test]
+        [TestCase("{[(])}")]
+        [TestCase("}][}}(}][))]")]
+        [TestCase("{)[](}]}]}))}(())(")]
+        [TestCase("([[)")]
+        public void When_BalancedNotBrackets_Should_ReturnNo(string input)
+        {
+            Assert.AreEqual("NO", _balancedBracketsSolver.IsBalanced(input));
+        }
+
+
+
+    }
+}
---
+++ Algorithms/Algorithms/stack/BalancedBracketsSolver.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.stack
+{
+    public class BalancedBracketsSolver: IBalancedBracketsSolver
+    {
+        public string IsBalanced(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            for (var i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+
+                if (c == '{' || c == '[' || c == '(')
+                {
+                    stack.Push(c);
+                }
+                else if (c == '}')
+                {
+                    if (stack.Count > 0 && stack.Peek() == '{')
+                    {
+                        stack.Pop();
+                    }
+                    else
+                        return "NO";
+                }
+                else if (c == ']')
+                {
+                    if (stack.Count > 0 && stack.Peek() == '[')
+                    {
+                        stack.Pop();
+                    }
+                    else
+                        return "NO";
+                }
+                else if (c == ')')
+                {
+                    if (stack.Count > 0 && stack.Peek() == '(')
+                    {
+                        stack.Pop();
+                    }
+                    else
+                        return "NO";
+                }
+            }
+
+            if (stack.Count == 0)
+            {
+                return "YES";
+            }
+            return "NO";
+        }
+    }
+}
