35851a069b877b72389d7d8c7b9f38bdf7b86e44
Muhammad Ahmad Saeed <ahmadsaeed90@gmail.com>
2020-05-29 22:48:41 +0700

    Create HitCounter.java

---
+++ Java/HitCounter.java
@@ -0,0 +1,24 @@
+class HitCounter {
+    Deque<Integer> queue;
+    /** Initialize your data structure here. */
+    public HitCounter() {
+        queue = new ArrayDeque<>();
+    }
+    
+    /** Record a hit.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    // hit: store timesstamp
+    public void hit(int timestamp) {
+        queue.offer(timestamp);
+    }
+    
+    /** Return the number of hits in the past 5 minutes.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    // getHits: we remove earilier timestamp based on the current timestamp, get the total size
+    public int getHits(int timestamp) {
+        while (!queue.isEmpty() && queue.peek() <= timestamp - 300) {
+            queue.poll();
+        }
+        return queue.size();
+    }
+}
