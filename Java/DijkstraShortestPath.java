b4fc099611fcae81a2a5bea776d4acb5ade756a0
Muhammad Ahmad Saeed <ahmadsaeed90@gmail.com>
2020-07-14 12:02:52 +0700

    add dikstra

---
+++ Java/DijkstraShortestPath.java
@@ -0,0 +1,93 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class DijkstraShortestPath {
+
+	public static int dijkstra(int N, List<List<Node>> adjList, int source, int dest) {
+
+		int[] dist = new int[N];
+		Arrays.fill(dist, Integer.MAX_VALUE);
+
+		boolean[] visited = new boolean[N];
+
+		var pq = new PriorityQueue<Node>();
+		dist[source] = 0;
+		pq.add(new Node(source, dist[source])); // start with source node
+
+		while (!pq.isEmpty()) {
+			Node u = pq.remove();
+
+			visited[u.node] = true;
+
+			var neighbours = adjList.get(u.node);
+
+			for (var v : neighbours) {
+				if (!visited[v.node]) {
+					var newNeigCost = dist[u.node] + v.cost;
+
+					if (newNeigCost < dist[v.node]) {
+						dist[v.node] = newNeigCost;
+
+						pq.add(new Node(v.node, dist[v.node]));
+					}
+				}
+			}
+		}
+		
+		return dist[dest];
+	}
+
+	public static void main(String[] args) {
+		int V = 5;
+		int source = 0;
+
+		// Adjacency list representation of the
+		// connected edges
+		List<List<Node>> adj = new ArrayList<List<Node>>();
+
+		// Initialize list for every node
+		for (int i = 0; i < V; i++) {
+			List<Node> item = new ArrayList<Node>();
+			adj.add(item);
+		}
+
+		adj.get(0).add(new Node(1, 9));
+		adj.get(0).add(new Node(2, 6));
+		adj.get(0).add(new Node(3, 5));
+		adj.get(0).add(new Node(4, 3));
+
+		adj.get(2).add(new Node(1, 2));
+		adj.get(2).add(new Node(3, 4));
+
+		int dest = 3;
+
+		// Calculate the single source shortest path
+		int res = dijkstra(V, adj, source, dest);
+
+		System.out.println("Shortest distance from " + source + " to " + dest + "=" + res);
+	}
+
+	static class Node implements Comparable<Node> {
+		public int node;
+		public int cost;
+
+		public Node() {
+		}
+
+		public Node(int node, int cost) {
+			this.node = node;
+			this.cost = cost;
+		}
+
+		@Override
+		public int compareTo(Node node2) {
+			if (this.cost < node2.cost)
+				return -1;
+			if (this.cost > node2.cost)
+				return 1;
+			return 0;
+		}
+	}
+}
--- Java/Temp.java
+++ Java/Temp.java
@@ -1,36 +1,24 @@
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class Temp {
 	public static void main(String[] args) {
-		var list = Arrays.asList(10);
-		
-		int element = 8;
-		
-		System.out.println(Collections.binarySearch(list, element));
-		System.out.println(binarySearch(list, element));
+
+		var pq = new PriorityQueue<TempNode>(new Comparator<TempNode>() {
+			@Override
+			public int compare(TempNode o1, TempNode o2) {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+		});
 	}
-	
-	private static int binarySearch(List<Integer> list, int timestamp) {
 
-		int lo = 0;
-		int hi = list.size() - 1;
-		int mid = 0;
-
-		while (lo <= hi) {
-			mid = (lo + hi) / 2;
-
-			var midStamp = list.get(mid);
-
-			if (midStamp == timestamp)
-				return mid;
-			else if (midStamp < timestamp)
-				lo = mid + 1;
-			else
-				hi = mid - 1;
-		}
-
-		return -lo-1;
+	class TempNode {
+		public int x;
+		public int y;
 	}
 }
